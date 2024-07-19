92b61ab818443bc04977216470e4d9f607c6012b
Arek <arkadiuszwojcik15@wp.pl>
2024-07-19 17:21:29 +0200

    Add new route for dashboard.component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 export const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
   { path: '**', component: PageNotFoundComponent }
 ];

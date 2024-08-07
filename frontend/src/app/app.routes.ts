428985c926b03e1999cc3bf63d1e66b177f2c615
Arkadiusz Wojcik <arkadiuszwojcik15@wp.pl>
2024-08-07 16:34:08 +0200

    Add basic components for the accounts and wallets page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { TransactionComponent } from './features/transaction/transaction.component';
+import { AccountsComponent } from './features/accounts/accounts.component';
 
 export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'transaction', component: TransactionComponent},
+  { path: 'accounts', component: AccountsComponent },
   { path: '**', component: PageNotFoundComponent }
 ];

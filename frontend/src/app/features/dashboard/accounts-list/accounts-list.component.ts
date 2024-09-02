40dbeb5785e77bfe2c3c84666c07d73ec6a4d211
Arkadiusz Wojcik <arkadiuszwojcik15@wp.pl>
2024-09-02 17:56:39 +0200

    Added data-table component to display data

--- frontend/src/app/features/dashboard/accounts-list/accounts-list.component.ts
+++ frontend/src/app/features/dashboard/accounts-list/accounts-list.component.ts
@@ -2,30 +2,64 @@ import { Component } from '@angular/core';
 
 import { AccountListHeaderComponent } from './account-list-header/account-list-header.component';
 import { AccountItemComponent } from './account-item/account-item.component';
+import { DataTableComponent } from '../../../shared/components/data-table/data-table.component';
+import { TableConfig } from '../../../shared/model/table-config.model';
+import { AccountType } from '../../accounts/account.model';
+import { AccountsService } from '../../accounts/accounts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-accounts-list',
   standalone: true,
   templateUrl: './accounts-list.component.html',
   styleUrl: './accounts-list.component.scss',
-  imports: [AccountListHeaderComponent, AccountItemComponent],
+  imports: [
+    AccountListHeaderComponent,
+    AccountItemComponent,
+    DataTableComponent,
+  ],
 })
 export class AccountsListComponent {
-  accountGroups = [
-    {
-      nameHeader: 'Account Name',
-      balanceHeader: 'Balance',
-      accounts: [{ name: 'Account 0', balance: 145758.3, currency: 'PLN' }],
-      currencyCode: 'PLN',
-    },
-    {
-      nameHeader: 'Wallet Name',
-      balanceHeader: 'Cash Amount',
-      accounts: [
-        { name: 'PL', balance: 5758.3, currency: 'PLN' },
-        { name: 'EU', balance: 1758.3, currency: 'EUR' },
-      ],
-      currencyCode: 'PLN',
-    },
+  private accountsSubscription!: Subscription;
+  accountsTableConfig!: TableConfig;
+  walletsTableConfig!: TableConfig;
+  accountType = AccountType;
+
+  constructor(private accountsService: AccountsService) {}
+
+  private accountTableColumns = [
+    { label: 'Account Name', key: 'name', width: 'col' },
+    { label: 'Balance', key: 'balance', width: 'col-2 text-end' },
   ];
+
+  private walletsTableColumns = [
+    { label: 'Wallet Name', key: 'name', width: 'col' },
+    { label: 'Cash Amount', key: 'balance', width: 'col-2 text-end' },
+  ];
+
+  ngOnInit() {
+    this.accountsSubscription = this.accountsService.accounts$.subscribe(
+      (accounts) => {
+        this.accountsTableConfig = {
+          columns: this.accountTableColumns,
+          data: accounts.filter(
+            (account) => account.accountType === AccountType.Bank
+          ),
+          showDropdownMenu: false,
+        };
+
+        this.walletsTableConfig = {
+          columns: this.walletsTableColumns,
+          data: accounts.filter(
+            (account) => account.accountType === AccountType.Wallet
+          ),
+          showDropdownMenu: false,
+        };
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.accountsSubscription.unsubscribe();
+  }
 }

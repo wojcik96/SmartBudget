2e6b575dc70fd5d3be65a3fa8f6ae867cb403668
Arkadiusz Wojcik <arkadiuszwojcik15@wp.pl>
2024-09-04 11:19:54 +0200

    Add new Column type enum

--- frontend/src/app/shared/model/table-config.model.ts
+++ frontend/src/app/shared/model/table-config.model.ts
@@ -1,11 +1,17 @@
+export enum ColumnType {
+  CURRENCY = 'currency',
+  STATUS = 'status',
+}
+
 export interface ColumnConfig {
-    label: string;
-    key: string;
-    width: string;
-  }
+  label: string
+  key: string
+  cssClass: string
+  type?: ColumnType
+}
 
 export interface TableConfig {
-    columns: ColumnConfig[];
-    data: { [key: string]: any }[];
-    showDropdownMenu: boolean;
-  }
+  columns: ColumnConfig[]
+  data: { [key: string]: any }[]
+  showDropdownMenu: boolean
+}

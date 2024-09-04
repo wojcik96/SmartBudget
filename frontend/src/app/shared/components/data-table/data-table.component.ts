d8b81ea7e4115fccf130d9ae7ead04a164a51a71
Arkadiusz Wojcik <arkadiuszwojcik15@wp.pl>
2024-09-04 11:19:32 +0200

    Improve the Data Table component

--- frontend/src/app/shared/components/data-table/data-table.component.ts
+++ frontend/src/app/shared/components/data-table/data-table.component.ts
@@ -2,17 +2,21 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { TableConfig } from '../../model/table-config.model';
 import { RowAction, RowActionType } from '../../model/row-action.model';
+import { CurrencyValueDirective } from '../../directives/currency-value.directive';
+import { ColumnType } from '../../model/table-config.model';
 
 @Component({
   selector: 'app-data-table',
   standalone: true,
   templateUrl: './data-table.component.html',
   styleUrl: './data-table.component.scss',
+  imports: [CurrencyValueDirective]
 })
 export class DataTableComponent {
   @Input({ required: true }) tableConfig!: TableConfig;
   @Output() selectedRow = new EventEmitter<RowAction>();
 
+  columnType = ColumnType;
   rowActionType = RowActionType;
 
   onClick(rowId: string, type: RowActionType) {
@@ -21,4 +25,5 @@ export class DataTableComponent {
       type: type,
     });
   }
+  
 }

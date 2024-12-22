497fac2c858b4b10badaeea949636ec6669d7784
Arkadiusz Wojcik <arkadiuszwojcik15@wp.pl>
2024-12-22 17:41:14 +0100

    Added imports array in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,12 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { CategoriesModule } from './categories/categories.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { BudgetsModule } from './budgets/budgets.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, TransactionsModule],
+  imports: [AuthModule, UsersModule, TransactionsModule, CategoriesModule, AccountsModule, BudgetsModule],
   controllers: [AppController],
   providers: [AppService],
 })
